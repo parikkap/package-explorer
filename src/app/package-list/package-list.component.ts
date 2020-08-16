4db596679712b6393f8085ee6edec2ab227ad72c
Peter Parikka <peter.parikka@frantic.com>
2020-08-16 21:13:39 +0300

    Refactor code

--- src/app/package-list/package-list.component.ts
+++ src/app/package-list/package-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data/data.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Package } from '../models/package';
 
 @Component({
   selector: 'app-package-list',
@@ -9,29 +10,35 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./package-list.component.scss'],
 })
 export class PackageListComponent implements OnInit {
-  public data$: Observable<any>;
+  public packageData$: Observable<any>;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.data$ = this.dataService.packageData.pipe(
-      map((rawText) => {
-        const splitByPackage = rawText.split('\n\n');
-        const packagePairsMatrix = splitByPackage.map((rawPackage) =>
-          rawPackage.split(/^(?=\w\D*:)/m),
-        );
-
-        const packageArray = packagePairsMatrix.map((pack) => {
-          const packageObj = {};
-          pack.forEach((pair) => {
-            const pairArray = pair.split(/:\s{1}(?=\w)/);
-            packageObj[pairArray[0].toLowerCase()] = pairArray[1];
-          });
-          return { ...packageObj };
-        });
-        return packageArray.map(item => console.log(item))
-      }),
+    this.packageData$ = this.dataService.packageData.pipe(
+      map((rawText) =>
+        this.constructPackageArray(rawText).sort((a: Package, b: Package) =>
+          a.package.toLowerCase().localeCompare(b.package.toLowerCase()),
+        ),
+      ),
     );
-    // this.data$.subscribe((item) => console.log(item));
+  }
+
+  private constructPackageArray(rawText: string): Array<Partial<Package>> {
+    const splitByPackage = rawText.split('\n\n');
+    const packagePairsMatrix = splitByPackage.map((rawPackage) =>
+      rawPackage.split(/^(?=\w\D*:)/m),
+    );
+    // Remove last item that is undefined
+    packagePairsMatrix.pop();
+    const packageArray = packagePairsMatrix.map((pack) => {
+      const packageObj = {};
+      pack.forEach((pair) => {
+        const pairArray = pair.split(/:\s{1}(?=\w)/);
+        packageObj[pairArray[0].toLowerCase()] = pairArray[1];
+      });
+      return { ...packageObj };
+    });
+    return packageArray;
   }
 }
